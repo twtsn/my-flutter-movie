835cb682cf41173469c0b9a3347d7614b91be034
xuyang01 <xuyang01@crop.netease.com>
2019-03-24 16:58:08 +0800

    '拖动加载'

--- lib/tabs/MovieList.dart
+++ lib/tabs/MovieList.dart
@@ -4,11 +4,10 @@ import 'dart:io';
 import 'package:flutter_app/tabs/Movie.dart';
 import 'package:flutter_app/tabs/Subject.dart';
 import 'package:flutter_app/common/MyEvent.dart';
+
 class MovieList extends StatefulWidget {
   MovieList({Key key, this.url, this.city, this.query, this.changeData}):super(key: key);
   String url;
-  int start = 0;
-  int count = 10;
   String city;
   String query;
   final Function(int total) changeData;
@@ -19,7 +18,11 @@ class MovieList extends StatefulWidget {
 }
 class _MovieListState extends State<MovieList> {
   ScrollController _scrollController = new ScrollController();
-  Movie moviePage = new Movie();
+  int start = 0;
+  int count = 10;
+  int totalSize = 0;
+  String loadMoreText = '';
+  List<Card> listCard = [];
   Future<Movie> future;
   @override
   void initState() {
@@ -29,9 +32,23 @@ class _MovieListState extends State<MovieList> {
     eventBus.on<MyEvent>().listen((MyEvent data) =>
         _onDataChange(data)
     );
+    _scrollEvent();
 //    _scrollController.addListener((){
 //      print(_scrollController.position.pixels);
 //    });
+  }
+  void _scrollEvent() {
+    _scrollController.addListener(() {
+      var maxScroll = _scrollController.position.maxScrollExtent;
+      var pixel = _scrollController.position.pixels;
+      if (maxScroll == pixel && listCard.length < totalSize) {
+        setState(() {
+          loadMoreText = "正在加载中...";
+        });
+        _refresh();
+      } else {
+      }
+    });
   }
   void _onDataChange (data) {
     setState((){
@@ -40,14 +57,55 @@ class _MovieListState extends State<MovieList> {
        future = _getOnlineMovieList(); //刷新数据,重新设置future就行了
     });
   }
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      body: new Center(
+//        child: FutureBuilder(
+//            builder: _buildFuture,
+//            future: future
+//        ),
+//      ),
+//    );
+//  }
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: FutureBuilder(
-        builder: _buildFuture,
-        future: future
+    return new Scaffold(
+      body: new Center(
+        child: RefreshIndicator(
+          onRefresh: _refresh,
+          child: new ListView.builder(
+            itemCount: listCard.length,
+            itemBuilder: (context,i) {
+              if(listCard.length == i + 1) {
+                return _buildProgressMoreIndicator();
+              }
+              return listCard[i];
+            },
+            controller: _scrollController,
+          )
+//          FutureBuilder(
+//              builder: _buildFuture,
+//              future: future
+//          ),
+        )
       ),
     );
+  }
+  Widget _buildProgressMoreIndicator() {
+    return new Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: new Center(
+        child: new Text(loadMoreText),
+      ),
+    );
+  }
+  Future<Null> _refresh() async {
+    print('refresh');
+    start += count;
+    print('start====' + start.toString());
+    _getOnlineMovieList();
+    return;
   }
   //利用FutureBuilder来实现懒加载，并可以监听加载过程的状态
   Widget _buildFuture(BuildContext context, AsyncSnapshot<Movie> snapshot) {
@@ -72,13 +130,16 @@ class _MovieListState extends State<MovieList> {
     }
   }
   Widget _createListView(BuildContext context,  AsyncSnapshot<Movie> snapshot) {
-    moviePage = snapshot.data;
-    List<Card> listView = _getList();
-    return ListView(
-        children: listView
-    );
+//    print('list length' + listCard.length.toString());
+//    return ListView(
+//        children: listCard
+//    );
   }
-  List<Card> _getList () {
+  _addMoreData(Movie movie) {
+    listCard.addAll(_getList(movie));
+    print('listCard' + listCard.length.toString());
+  }
+  List<Card> _getList (Movie moviePage) {
     List<Card> list = [];
     Card newItem = null;
     List<Subject> subjectList = moviePage.subjectList;
@@ -104,16 +165,32 @@ class _MovieListState extends State<MovieList> {
     );
   }
    Future<Movie>_getOnlineMovieList () async {
+     setState(() {
+       loadMoreText = "正在加载数据...";
+     });
     var http = new HttpClient();
     print('请求参数 url ===============' + widget.url);
     print('请求参数 city ===============' + widget.city);
-    String url = widget.url + '?city='+ widget.city +'&start='+  widget.start.toString() +'&count=' + widget.count.toString();
+    print('请求参数 start ===============' + start.toString());
+    String url = widget.url + '?city='+ widget.city +'&start='+  start.toString() +'&count=' + count.toString();
     var request = await http.getUrl(Uri.parse(url));
     var response = await request.close();
     if (response.statusCode == HttpStatus.ok){
       var jsonData = await response.transform(utf8.decoder).join();
       Movie movie = Movie.getData(json.decode(jsonData));
-      widget.changeData(movie.total);
+      if(start == 0) {
+        totalSize = movie.total;
+        widget.changeData(movie.total);
+      }
+      _addMoreData(movie);
+
+      var text = "点击加载更多";
+      if (movie.total == listCard.length) {
+        text = '无更多数据';
+      }
+      setState(() {
+        loadMoreText = text;
+      });
       return movie;
     } else {
       return null;
